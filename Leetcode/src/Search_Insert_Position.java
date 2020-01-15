eaf9377056f068cb98760712ffa652f3ea5eaa91
ddyy814 <43596027+ddyy814@users.noreply.github.com>
2020-01-15 00:03:08 -0800

    Update Search_Insert_Position.java

--- Leetcode/src/Search_Insert_Position.java
+++ Leetcode/src/Search_Insert_Position.java
@@ -28,7 +28,7 @@ public class Search_Insert_Position {
     public int searchInsert(int[] nums, int target){
         int low = 0; int high = nums.length - 1;
         while(low <= high){
-            int mid = low + (low + high) / 2;
+            int mid = (low + high) / 2;
             if(nums[mid] == target) return mid;
             else if(nums[mid] > target)
                 high = mid - 1;
