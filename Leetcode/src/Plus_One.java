9a6345747440c925260e7004ddf4979a9ca4b965
ddyy814 <43596027+ddyy814@users.noreply.github.com>
2020-01-15 17:27:40 -0800

    Update Plus_One.java

--- Leetcode/src/Plus_One.java
+++ Leetcode/src/Plus_One.java
@@ -37,7 +37,7 @@ public class Plus_One {
         if(carry == 1){
             int[] result = new int[digits.length + 1];
             result[0] = 1;
-            for(int i = 0; i < result.length; i++){
+            for(int i = 1; i < result.length; i++){
                 result[i] = digits[i - 1];
             }
             return result;
